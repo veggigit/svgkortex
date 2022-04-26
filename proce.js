e5d0cb8993289bce0b92ed9b09f82d6905f07fef
veggigit <estebancajina@gmail.com>
2022-04-26 17:58:58 -0400

    Path data proce ok

--- proce.js
+++ proce.js
@@ -3,18 +3,17 @@ class svgProce {
     domResultados;
     domSVGloaded;
     // dom para cargar data. Plz usar ids
-    constructor(domMainID){
+    constructor(domMainID) {
         this.domMain = document.getElementById(domMainID);
         this.mount();
     }
 
-    svgRead (file)
-    {
+    svgRead(file) {
         let reader = new FileReader();
 
         reader.readAsText(file);
 
-        reader.onload = ()=> {
+        reader.onload = () => {
             this.domSVGloaded.innerHTML = reader.result;
             // var paths = document.getElementsByTagName('path');
             // var circles = document.getElementsByTagName('circle');
@@ -23,53 +22,75 @@ class svgProce {
             // var lines = document.getElementsByTagName('line');
             // var polygons = document.getElementsByTagName('polygon');
             this.svgCirclesHandle();
+            this.svgPathHandle();
         };
 
         reader.onerror = () => {
             console.log(reader.error);
-          };
+        };
     }
 
-    svgCirclesHandle(){
-        let circles = document.getElementsByTagName('circle');
+    svgPathHandle() {
+        let pathElements = document.getElementsByTagName('path');
+
+        if (pathElements.length != 0) {
+            //objt
+            let sheetPaths = new Object();
+            sheetPaths.qty = pathElements.length;
+            sheetPaths.long = 0;
+            // dom
+            var domPathSheet = document.createElement('div');
+            domPathSheet.setAttribute('class', 'path-sheet');
+            // proce
+            let arrPaths = Array.from(pathElements);
+            arrPaths.map(function (path) {
+                sheetPaths.long += path.getTotalLength();
+            });
+            //show data
+            this.domResultados.appendChild(domPathSheet);
+            domPathSheet.innerHTML = '<h3>Info paths</h3><ul><li>Cantidad: ' + sheetPaths.qty + '</li><li>Total lineal: ' + sheetPaths.long + '</li></ul>';
+        } else {
+            console.log('svg no tiene paths');
+        }
+
+
+    }
+
+    svgCirclesHandle() {
+        let circleElements = document.getElementsByTagName('circle');
         let getcircunferencia = r => 2 * 3.14 * r;
 
-        if(circles.length != 0)
-        {
+        if (circleElements.length != 0) {
             // obj
             let sheetCircles = new Object();
-            sheetCircles.qty = circles.length;
+            sheetCircles.qty = circleElements.length;
             sheetCircles.long = 0;
             // dom
             var domCircSheet = document.createElement('div');
             domCircSheet.setAttribute('class', 'circ-sheet');
-        
-            let arrCircles = Array.from(circles);
-            arrCircles.map(function(e){
+            // proce
+            let arrCircles = Array.from(circleElements);
+            arrCircles.map(function (e) {
                 let r = e.getAttribute('r');
-                sheetCircles.long+=getcircunferencia(r);
+                sheetCircles.long += getcircunferencia(r);
             });
-
+            // show data
             this.domResultados.appendChild(domCircSheet);
-            domCircSheet.innerHTML = '<h3>Info circulos</h3><ul><li>Cantidad: '+sheetCircles.qty+'</li><li>Total lineal: '+sheetCircles.long+'</li></ul>';
+            domCircSheet.innerHTML = '<h3>Info circulos</h3><ul><li>Cantidad: ' + sheetCircles.qty + '</li><li>Total lineal: ' + sheetCircles.long + '</li></ul>';
         }
-        else
-        {
+        else {
             console.log('svg no tiene circles');
         }
-
     }
 
-    mount(){
-        if(this.domMain.length != 0)
-        {
+    mount() {
+        if (this.domMain.length != 0) {
             console.log('dom encontrado. Montando');
             this.domMain.innerHTML = "<div id='resultado'></div><div id='svgloaded'></div>";
             this.domResultados = document.getElementById('resultado');
             this.domSVGloaded = document.getElementById('svgloaded');
         }
-        else
-        {
+        else {
             console.log('no se pudo montar');
         }
     }
